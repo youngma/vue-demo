53fb80e614886a526bd69c4cb2b69b558eb79e37
seo minkyu <sym221@cashwali.io>
2021-04-15 14:39:49 +0900

    Add : Core UI (Admin Template)

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style lang="scss">
-@import "~@coreui/coreui/scss/coreui";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import 'assets/scss/style';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue';
-import CoreuiVue, { CSwitch, CButton } from '@coreui/vue';
+import CoreuiVue from '@coreui/vue';
 import App from './App.vue';
+import router from './router';
+import store from './store/store';
+import { iconsSet as icons } from './assets/icons/icons';
 
+Vue.prototype.$log = console.log.bind(console);
 Vue.use(CoreuiVue);
-Vue.component('CButton', CButton);
-Vue.component('CSwitch', CSwitch);
-
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  icons,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
